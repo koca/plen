11284d59a7e13785c9e5861709145c4a3bde28d5
Mesut Koca <imesutkoca@gmail.com>
2018-09-13 01:54:29 +0300

    updatee color

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   lintOnSave: false,
 
+  pwa: {
+    themeColor: "#673AB7"
+  },
   configureWebpack: config => {
     //workerize-loader issue
     //https://github.com/webpack/webpack/issues/6642#issuecomment-371087342
